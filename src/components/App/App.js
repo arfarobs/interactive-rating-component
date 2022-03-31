51b3fb3e51491074b1a6e9dd53d407d98eb93a4e
arfarobs <arthurroberts@mail.com>
2022-04-01 07:30:48 +0800

    Changed repo. Styled mobile.

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Rating } from '../Rating/Rating';
+import Thanks from '../Thanks/Thanks';
+
+function App() {
+  return (
+    <article>
+      <Rating />
+    </article>
+  );
+}
+
+export default App;
---
+++ src/components/Rating/Rating.js
@@ -0,0 +1,38 @@
+import star from './icon-star.svg';
+import './Rating.css';
+
+export const Rating = () => {
+  return (
+    <>
+      <img src={star} alt="" aria-hidden="true" />
+      <h1>How did we do?</h1>
+      <p>
+        Please let us know how we did with your support request. All feedback is appreciated 
+        to help us improve our offering!
+      </p>
+      <form>
+        <label htmlFor="one" className="spacer" id="one">
+          <input name="rating" type="radio" value="1"/>
+          1
+        </label>
+        <label htmlFor="two" className="spacer">
+          <input name="rating" type="radio" value="2"/>
+          2
+        </label>
+        <label htmlFor="three" className="spacer">
+          <input name="rating" type="radio" value="3"/>
+          3
+        </label>
+        <label htmlFor="four" className="spacer">
+          <input name="rating" type="radio" value="4"/>
+          4
+        </label>
+        <label htmlFor="five">
+          <input name="rating" type="radio" value="5"/>
+          5
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
+  )
+}
